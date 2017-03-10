b9562ef109a523820de9d92d776cb1f255a25c46
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-03-10 10:37:47 -0500

    Implement -a flag to show hidden files

--- gols.go
+++ gols.go
@@ -1,28 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
-func printDir(dir string, hidden bool) error {
+func printDir(dir string, all bool) error {
 	info, err := ioutil.ReadDir(dir)
 	if err != nil {
 		return err
 	}
 	for _, dir := range info {
-		if []rune(dir.Name())[0] != 46 {
+		if all {
 			fmt.Println(dir.Name())
+		} else {
+			if []rune(dir.Name())[0] != 46 {
+				fmt.Println(dir.Name())
+			}
 		}
 	}
 	return nil
 }
 
 func main() {
-	if len(os.Args) > 1 {
-		err := printDir(os.Args[1], false)
+	// Command Line Options
+	allBool := flag.Bool("a", false, "do not ignore entries starting with .")
+
+	flag.Parse()
+
+	if len(flag.Args()) > 0 {
+		err := printDir(flag.Args()[0], *allBool)
 		if err != nil {
 			log.Println(err)
 		}
@@ -31,7 +41,7 @@ func main() {
 		if err != nil {
 			log.Println(err)
 		}
-		err = printDir(dir, false)
+		err = printDir(dir, *allBool)
 		if err != nil {
 			log.Println(err)
 		}
